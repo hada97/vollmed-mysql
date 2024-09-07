b873ef02c45cd1c3365a48f83bd0929cc7ccc8d7
Adailson <adailson65@gmail.com>
2024-09-07 17:06:24 -0300

    add class SpringDocConfigurations

--- src/main/java/med/voll/api/SwaggerConfig.java
+++ src/main/java/med/voll/api/SwaggerConfig.java
@@ -4,17 +4,20 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
+    @Primary
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title( "API do Ada em Produção")
+                        .title( "API Vollmed em Produção")
                         .version("v1")
-                        .description("API Rest desenvolvida no curso da Alura")
+                        .description("API Rest desenvolvida por A. Guimaraes" +
+                                "LOGIN OBRIGATORIO")
                 );
     }
 }
