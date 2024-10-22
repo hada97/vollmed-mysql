13344a1eda257e35b35894297c11f94d57ac0817
Adailson <adailson65@gmail.com>
2024-10-22 14:48:45 -0300

    up

--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -1,9 +1,8 @@
 package med.voll.api.infra.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import med.voll.api.infra.security.SecurityFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -16,51 +15,63 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 
-    @Configuration
-    @EnableWebSecurity
-    public class SecurityConfigurations {
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
 
-        @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-            http
-                    .csrf(csrf -> csrf.disable())  // Desativa a proteção CSRF
-                    .sessionManagement(session -> session
-                            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                    )
-                    .authorizeHttpRequests(authz -> authz
-                            .requestMatchers(HttpMethod.POST, "/login").permitAll()
-                            .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
-                            .anyRequest().authenticated()
-                    )
-                    .addFilterBefore(securityFilter(), UsernamePasswordAuthenticationFilter.class);
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())  // Desativa a proteção CSRF
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // Adiciona a configuração de CORS
+                .addFilterBefore(securityFilter(), UsernamePasswordAuthenticationFilter.class);
 
-            return http.build();
-        }
+        return http.build();
+    }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedOrigin("http://127.0.0.1:5500"); // URL do seu frontend
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
 
-        @Bean
-        public CsrfTokenRepository csrfTokenRepository() {
-            return new HttpSessionCsrfTokenRepository();
-        }
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
-        @Bean
-        public SecurityFilter securityFilter() {
-            // Definição do seu SecurityFilter
-            return new SecurityFilter();
-        }
+    @Bean
+    public CsrfTokenRepository csrfTokenRepository() {
+        return new HttpSessionCsrfTokenRepository();
+    }
+
+    @Bean
+    public SecurityFilter securityFilter() {
+        return new SecurityFilter();
+    }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
-           return configuration.getAuthenticationManager();
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
 }
-
-
