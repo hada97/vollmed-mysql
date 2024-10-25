d8cb5759006e4bf2cd2531143becde1b8fa385e9
Adailson <adailson65@gmail.com>
2024-10-24 23:42:52 -0300

    add especialidade dadoslistagem consulta

--- src/main/java/med/voll/api/domain/consulta/DadosListagemConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/DadosListagemConsulta.java
@@ -1,11 +1,13 @@
 package med.voll.api.domain.consulta;
 
+import med.voll.api.domain.medico.Especialidade;
+
 import java.time.LocalDateTime;
 
-public record DadosListagemConsulta(Long id, Long idMedico, Long idPaciente, LocalDateTime data) {
+public record DadosListagemConsulta(Long id, Long idMedico, Long idPaciente, LocalDateTime data, Especialidade especialidade) {
 
     public DadosListagemConsulta(Consulta consulta) {
-        this(consulta.getId(), consulta.getMedico().getId(), consulta.getPaciente().getId(), consulta.getData());
+        this(consulta.getId(), consulta.getMedico().getId(), consulta.getPaciente().getId(), consulta.getData(),consulta.getMedico().getEspecialidade());
     }
 
 }
